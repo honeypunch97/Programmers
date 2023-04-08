c57e83b0a0038b7a67c8ea89965d91f10a66a9af
jongwoo <57937641+honeypunch97@users.noreply.github.com>
2023-04-08 23:34:14 +0900

    [level 0] Title: 옷가게 할인 받기, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120818. 옷가게 할인 받기/옷가게 할인 받기.js
@@ -0,0 +1,11 @@
+function solution(price) {
+   let discountRate = 0;
+   if (price >= 500000) {
+      discountRate = 0.2;
+   } else if (price >= 300000) {
+      discountRate = 0.1;
+   } else if (price >= 100000) {
+      discountRate = 0.05;
+   }
+   return Math.floor(price - price * discountRate);
+}
