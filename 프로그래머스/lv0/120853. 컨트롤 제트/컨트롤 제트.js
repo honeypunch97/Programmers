bc85f522a2ee950bdf0797f8aaa73656d209e78c
jongwoo <57937641+honeypunch97@users.noreply.github.com>
2023-04-12 20:32:43 +0900

    [level 0] Title: 컨트롤 제트, Time: 0.21 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120853. 컨트롤 제트/컨트롤 제트.js
@@ -0,0 +1,13 @@
+function solution(s) {
+   let answer = 0;
+   let minusIdx = 0;
+   let arr = s.split(' ');
+   for (let i = 0; i < arr.length; i++) {
+      if (arr[i] === 'Z') {
+         answer -= Number(arr[i - 1]);
+      } else {
+         answer += Number(arr[i]);
+      }
+   }
+   return answer;
+}
