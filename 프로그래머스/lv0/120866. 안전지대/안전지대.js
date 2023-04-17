db6451a62ac1ab7e6c989d5a0ab2a00413df6946
jongwoo <57937641+honeypunch97@users.noreply.github.com>
2023-04-17 20:39:38 +0900

    [level 0] Title: 안전지대, Time: 0.08 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120866. 안전지대/안전지대.js
+++ 프로그래머스/lv0/120866. 안전지대/안전지대.js
@@ -1,24 +1,29 @@
 function solution(board) {
   const n = board.length;
-  let answer = 0;
-  
-  // 상하좌우, 대각선까지 모두 체크
   const dx = [-1, 0, 1, 0, -1, 1, 1, -1];
   const dy = [0, 1, 0, -1, 1, 1, -1, -1];
+  let answer = 0;
   
+  // 각 좌표를 하나씩 순회
   for (let i = 0; i < n; i++) {
     for (let j = 0; j < n; j++) {
-      if (board[i][j] === 1) continue; // 지뢰인 경우 skip
-      let cnt = 0;
-      for (let k = 0; k < 8; k++) {
-        const nx = i + dx[k];
-        const ny = j + dy[k];
-        if (nx < 0 || nx >= n || ny < 0 || ny >= n) continue; // 배열 범위 벗어나는 경우 skip
-        if (board[nx][ny] === 1) cnt++; // 주변에 지뢰가 있는 경우 count
+      // 현재 좌표가 지뢰가 아니라면
+      if (board[i][j] === 0) {
+        let count = 0;
+        // 주변 8개 좌표를 확인하여 지뢰가 있는지 확인
+        for (let k = 0; k < 8; k++) {
+          const nx = i + dx[k];
+          const ny = j + dy[k];
+          if (nx < 0 || nx >= n || ny < 0 || ny >= n) continue;
+          if (board[nx][ny] === 1) count++;
+        }
+        // 지뢰가 없다면 안전한 칸으로 간주
+        if (count === 0) answer++;
       }
-      answer += cnt === 0 ? 1 : 0; // 안전한 칸일 경우 answer에 1을 더함
     }
   }
   
   return answer;
 }
+
+// 이 코드는 혼자 생각한 코드가 아님. 정답들을 보고 짠코드임 다시풀예정
