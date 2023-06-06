0fa13c6c5f25edb8b2fbb58156f252f9fe742547
jongwoo <57937641+honeypunch97@users.noreply.github.com>
2023-06-06 20:34:38 +0900

    [level 2] Title: 최솟값 만들기, Time: 0.78 ms, Memory: 33 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12941. 최솟값 만들기/최솟값 만들기.js
@@ -0,0 +1,7 @@
+function solution(A, B) {
+   let answer = 0;
+   let arrA = A.sort((a, b) => a - b);
+   let arrB = B.sort((a, b) => b - a);
+   for (let i = 0; i < arrA.length; i++) answer += arrA[i] * arrB[i];
+   return answer;
+}
