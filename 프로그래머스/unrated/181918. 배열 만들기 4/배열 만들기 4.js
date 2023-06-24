406bb26227b4ac24ed14606bf9b3a7a5adc1c4bd
jongwoo <57937641+honeypunch97@users.noreply.github.com>
2023-06-24 15:34:35 +0900

    [unrated] Title: 배열 만들기 4, Time: 4.07 ms, Memory: 36.9 MB -BaekjoonHub

--- 프로그래머스/unrated/181918. 배열 만들기 4/배열 만들기 4.js
+++ 프로그래머스/unrated/181918. 배열 만들기 4/배열 만들기 4.js
@@ -2,14 +2,10 @@ function solution(arr) {
    let stk = [];
    let i = 0;
    while (i < arr.length) {
-      if (stk.length === 0) {
-         stk.push(arr[i]);
-         i++;
-      } else {
-         if (stk.at(-1) < arr[i]) {
-            stk.push(arr[i]);
-            i++;
-         } else stk.pop();
+      if (stk.length === 0) stk.push(arr[i++]);
+      else {
+         if (stk.at(-1) < arr[i]) stk.push(arr[i++]);
+         else stk.pop();
       }
    }
    return stk;
