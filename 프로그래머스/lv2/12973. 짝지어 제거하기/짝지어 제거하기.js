f314258e61ae107c498eb6d888939f2cb3bcb355
jongwoo <57937641+honeypunch97@users.noreply.github.com>
2023-07-19 10:03:12 +0900

    [level 2] Title: 짝지어 제거하기, Time: 26.80 ms, Memory: 46.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12973. 짝지어 제거하기/짝지어 제거하기.js
@@ -0,0 +1,7 @@
+function solution(s) {
+   const stack = [];
+   for (let i = 0; i < s.length; i++) {
+      stack[stack.length - 1] === s[i] ? stack.pop() : stack.push(s[i]);
+   }
+   return stack.length === 0 ? 1 : 0;
+}
