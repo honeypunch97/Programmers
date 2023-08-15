cf6d7c25b6420968d5d70205e16cc84a80bffdf0
jongwoo <57937641+honeypunch97@users.noreply.github.com>
2023-08-15 23:17:14 +0900

    [Bronze III] Title: ACM 호텔, Time: 124 ms, Memory: 9368 KB -BaekjoonHub

--- 백준/Bronze/10250. ACM 호텔/ACM 호텔.ts
+++ 백준/Bronze/10250. ACM 호텔/ACM 호텔.ts
@@ -7,16 +7,10 @@ const solution = (input: string[]) => {
    let num: number = parseInt(input[0]);
    for (let i: number = 1; i <= num; i++) {
       const [H, W, N] = input[i].split(' ').map((item: string) => parseInt(item));
-      let floor: string = '';
-      if (N % H === 0) {
-         floor = String(H);
-      } else {
-         floor = String(N % H);
-      }
+      const floor: string = N % H === 0 ? String(H) : String(N % H);
       const roomNum: string = String(Math.ceil(N / H)).padStart(2, '0');
       result.push(floor + roomNum);
    }
    return result.join('\n');
 };
-
 console.log(solution(input));
