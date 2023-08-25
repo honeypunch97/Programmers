8f0e2f46e2676fc1d6bad7e3ca86c2a691e34b89
jongwoo <57937641+honeypunch97@users.noreply.github.com>
2023-08-25 23:20:01 +0900

    [Bronze V] Title: 콜센터, Time: 60 ms, Memory: 5320 KB -BaekjoonHub

---
+++ 백준/Bronze/5339. 콜센터/콜센터.cs
@@ -0,0 +1,17 @@
+using System;
+
+class Program
+{
+    static void Main() {
+        Console.WriteLine(@"     /~\
+    ( oo|
+    _\=/_
+   /  _  \
+  //|/.\|\\
+ ||  \ /  ||
+============
+|          |
+|          |
+|          |");
+    }
+}
