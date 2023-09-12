1d25f42dd7337e7c591578f4b04445e2860effdf
jongwoo <57937641+honeypunch97@users.noreply.github.com>
2023-09-12 19:57:57 +0900

    [Bronze IV] Title: 모음의 개수, Time: 64 ms, Memory: 5456 KB -BaekjoonHub

--- 백준/Bronze/1264. 모음의 개수/모음의 개수.cs
+++ 백준/Bronze/1264. 모음의 개수/모음의 개수.cs
@@ -1,38 +1,32 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
 
-namespace ConsoleApp49
+class Program
 {
-    class Program
+    static void Main()
     {
-        static void Main(string[] args)
+        while (true)
         {
-            while (true)
+            string line = Console.ReadLine();
+            if (line == "#") break;
+            int vowelCount = CountVowels(line);
+            Console.WriteLine(vowelCount);
+        }
+    }
+
+    static int CountVowels(string input)
+    {
+        int count = 0;
+        string vowels = "aeiouAEIOU";
+        
+        foreach (char c in input)
+        {
+            if (vowels.Contains(c))
             {
-                string s = Console.ReadLine().ToUpper();
-
-                int cnt = 0;
-
-                if (s.Equals("#"))
-                {
-                    return;
-                }
-                else
-                {
-                    for (int i = 0; i < s.Length; i++)
-                    {
-                        if (s[i].Equals('A') || s[i].Equals('E') || s[i].Equals('I') || s[i].Equals('U') || s[i].Equals('O'))
-                        {
-                            cnt++;
-                        }
-                    }
-
-                    Console.WriteLine(cnt);
-                }
+                count++;
             }
         }
+
+        return count;
     }
 }
