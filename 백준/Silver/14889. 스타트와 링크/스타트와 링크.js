1d3287aa4e10b520431b3ada12990db2acc41583
jongwoo <57937641+honeypunch97@users.noreply.github.com>
2024-03-01 23:31:41 +0900

    [Silver I] Title: 스타트와 링크, Time: 420 ms, Memory: 12336 KB -BaekjoonHub

---
+++ 백준/Silver/14889. 스타트와 링크/스타트와 링크.js
@@ -0,0 +1,46 @@
+const sol = (input) => {
+  const N = +input[0];
+  const halfN = N / 2;
+  const stats = input.slice(1).map((str) => str.split(" ").map(Number));
+
+  const check = new Array(N).fill(0);
+  let min = Number.MAX_SAFE_INTEGER;
+  function dfs(L, K) {
+    if (L === halfN) {
+      const sTeam = [];
+      const lTeam = [];
+      let sSum = (lSum = 0);
+      for (let i = 0; i < N; i++) {
+        if (check[i]) sTeam.push(i); 
+        else lTeam.push(i);
+      }
+      for (let i = 0; i < halfN; i++) {
+        for (let j = i + 1; j < halfN; j++) {
+          sSum = sSum + stats[sTeam[i]][sTeam[j]] + stats[sTeam[j]][sTeam[i]];
+          lSum = lSum + stats[lTeam[i]][lTeam[j]] + stats[lTeam[j]][lTeam[i]];
+        }
+      }
+      min = Math.min(min, Math.abs(sSum - lSum));
+      return;
+    }
+
+    for (let i = K; i < N; i++) {
+      check[i] = 1;
+      dfs(L + 1, i + 1);
+      check[i] = 0;
+    }
+  }
+  dfs(0, 0);
+  return min;
+};
+
+const input = [];
+require("readline")
+  .createInterface(process.stdin, process.stdout)
+  .on("line", (line) => {
+    input.push(line);
+  })
+  .on("close", () => {
+    console.log(sol(input));
+    process.exit();
+  });
