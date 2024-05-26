45980018508af37b12cbe6383cb8131d9fa35325
jongwoo <57937641+honeypunch97@users.noreply.github.com>
2024-05-26 21:48:32 +0900

    [level 1] Title: 덧칠하기, Time: 1.55 ms, Memory: 37.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/161989. 덧칠하기/덧칠하기.js
@@ -0,0 +1,13 @@
+function solution(n, m, sections) {
+    let answer = 0;
+    let painted = 0;
+    
+    sections.forEach(section => {
+        if (painted < section) {
+            answer++;
+            painted = section + m - 1;
+        }
+    });
+
+    return answer;
+}
