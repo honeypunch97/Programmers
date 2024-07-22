0e015cb37781733a14c29f663676380cf9707938
jongwoo <57937641+honeypunch97@users.noreply.github.com>
2024-07-22 22:34:26 +0900

    [level 2] Title: 숫자 변환하기, Time: 2699.48 ms, Memory: 53.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/154538. 숫자 변환하기/숫자 변환하기.js
@@ -0,0 +1,14 @@
+function solution(x, y, n) {
+    let answer = 0;
+    
+    let que = [[y, 0]];
+    
+    while(que.length) {
+        const [num, i] = que.shift();
+        if(num === x) return i;
+        if(num%2 === 0 && num/2 >= x) que.push([num/2, i+1]);
+        if(num%3 === 0 && num/3 >= x) que.push([num/3, i+1]);
+        if(num-n >= x) que.push([num-n, i+1]);
+    }
+    return -1;
+}
